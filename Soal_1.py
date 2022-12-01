4fb6b5eccf86075d46459b4ba101af56ece72fbf
Via Angelyca <117884184+Jaketus0@users.noreply.github.com>
2022-12-01 13:01:38 +0700

    Add files via upload

--- Soal_1.py
+++ Soal_1.py
@@ -6,32 +6,21 @@ print("4. Divide") #/
 pil= int(input("Enter choice(1/2/3/4): "))
 bil1= int(input("Enter first number: "))
 bil2= int(input("Enter second number: "))
-if pil==1:
-    def add():
-    tambah= bil1+bil2
-    print(bil1 ,"+", bil2, "=",tambah);
-    bil1= int(input("Enter first number: "))
-    bil2= int(input("Enter second number: "))
-    add()
-elif pil==2:
-    def subtract():
-    kurang= bil1-bil2
-    print(bil1, "-", bil2, "=",kurang);
-    bil1= int(input("Enter first number: "))
-    bil2= int(input("Enter second number: "))
-    subtract()
-elif pil==3:
-    kali= bil1*bil2
-    def multiply():
+def add():
+    if pil==1:
+        tambah= bil1+bil2
+        print(bil1 ,"+", bil2, "=",tambah);
+def subtract():
+    if pil==2:
+        kurang= bil1-bil2
+        print(bil1, "-", bil2, "=",kurang);
+def multiply():
+    if pil==3:
+        kali= bil1*bil2
         print(bil1 ,"*" ,bil2 ,"=",kali);
-        bil1= int(input("Enter first number: "))
-        bil2= int(input("Enter second number: "))
-    multiply()
-elif pil==4:
-    bagi= bil1/bil2
-    def divide():
+def divide():
+    if pil==4:
+        bagi= bil1/bil2
         print(bil1 ,"/", bil2, "=",bagi);
-        bil1= int(input("Enter first number: "))
-        bil2= int(input("Enter second number: "))
-    divide()
+
 
