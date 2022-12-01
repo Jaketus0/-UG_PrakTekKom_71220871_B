0668ba273a53e9d7c493c9307c374c133ddca2df
Via Angelyca <117884184+Jaketus0@users.noreply.github.com>
2022-12-01 13:16:54 +0700

    Add files via upload

--- Soal_1.py
+++ Soal_1.py
@@ -4,23 +4,33 @@ print("2. Subtract") #-
 print("3. Multiply") #*
 print("4. Divide") #/
 pil= int(input("Enter choice(1/2/3/4): "))
-bil1= int(input("Enter first number: "))
-bil2= int(input("Enter second number: "))
 def add():
-    if pil==1:
-        tambah= bil1+bil2
-        print(bil1 ,"+", bil2, "=",tambah);
+    tambah= bil1+bil2
+    print(bil1 ,"+", bil2, "=",tambah)
 def subtract():
-    if pil==2:
-        kurang= bil1-bil2
-        print(bil1, "-", bil2, "=",kurang);
+    kurang= bil1-bil2
+    print(bil1, "-", bil2, "=",kurang)
 def multiply():
-    if pil==3:
-        kali= bil1*bil2
-        print(bil1 ,"*" ,bil2 ,"=",kali);
+    kali= bil1*bil2
+    print(bil1 ,"*" ,bil2 ,"=",kali)
 def divide():
-    if pil==4:
-        bagi= bil1/bil2
-        print(bil1 ,"/", bil2, "=",bagi);
+    bagi= bil1/bil2
+    print(bil1 ,"/", bil2, "=",bagi)
+if pil==1:
+    bil1=float(input("Enter first number: "))
+    bil2=float(input("Enter second number: "))
+    add()
+elif pil==2:
+    bil1=float(input("Enter first number: "))
+    bil2=float(input("Enter second number: "))
+    subtract()
+elif pil==3:
+    bil1=float(input("Enter first number: "))
+    bil2=float(input("Enter second number: "))
+    multiply()
+elif pil==4:
+    bil1=float(input("Enter first number: "))
+    bil2=float(input("Enter second number: "))
+    divide()
 
 
